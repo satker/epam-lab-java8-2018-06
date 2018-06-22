2c25ee4cc8c5656e51913be8a049fb51757260ed
Artem_Kunats <Artem_Kunats@epam.com>
2018-06-22 14:54:30 +0300

    Done lambda.part1.exercise2

--- src/test/java/lambda/part1/exercise/Exercise2.java
+++ src/test/java/lambda/part1/exercise/Exercise2.java
@@ -28,20 +28,26 @@ public class Exercise2 {
 
     @Test
     public void implementsIntegerMultiplierUsingAnonymousClass() {
-        Multiplier<Integer> multiplier = null;
-
+        Multiplier<Integer> multiplier = new Multiplier<Integer>() {
+            @Override
+            public Integer multiply(Integer value, int multiplier) {
+                return value * multiplier;
+            }
+        };
         testIntegerMultiplier(multiplier);
     }
 
     @Test
     public void implementsMultiplierUsingStatementLambda() {
-        Multiplier<Integer> multiplier = null;
-
-        testIntegerMultiplier(multiplier);    }
+        Multiplier<Integer> multiplier = (value, valueMultiplier) -> {
+            return value * valueMultiplier;
+        };
+        testIntegerMultiplier(multiplier);
+    }
 
     @Test
     public void implementsIntegerMultiplierUsingExpressionLambda() {
-        Multiplier<Integer> multiplier = null;
+        Multiplier<Integer> multiplier = (value, valueMultiplier) -> value * valueMultiplier;
 
         testIntegerMultiplier(multiplier);
     }
@@ -56,7 +62,7 @@ public class Exercise2 {
 
     @Test
     public void implementsStringMultiplierUsingClassMethodReference() {
-        Multiplier<String> multiplier = null;
+        Multiplier<String> multiplier = Exercise2::multiplyString;
 
         assertEquals("aaa", multiplier.multiply("a", 3));
         assertEquals("", multiplier.multiply("qwerty", 0));
@@ -77,7 +83,7 @@ public class Exercise2 {
 
     @Test
     public void implementsStringMultiplierUsingObjectMethodReference() {
-        Multiplier<String> multiplier = null;
+        Multiplier<String> multiplier = this::stringSumWithDelimiter;
 
         assertEquals("a-a-a", multiplier.multiply("a", 3));
         assertEquals("", multiplier.multiply("qwerty", 0));
