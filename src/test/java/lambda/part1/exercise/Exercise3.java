8eb44792d98611be1755792f7c863eb8183f4c26
Artem_Kunats <Artem_Kunats@epam.com>
2018-06-22 15:13:19 +0300

    Done lambda.part1.exercise3

--- src/test/java/lambda/part1/exercise/Exercise3.java
+++ src/test/java/lambda/part1/exercise/Exercise3.java
@@ -4,8 +4,10 @@ import lambda.data.Person;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
+import static com.google.common.collect.FluentIterable.from;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
@@ -17,6 +19,9 @@ public class Exercise3 {
         Person[] persons = getPersons();
 
         // TODO использовать Arrays.sort + expression-lambda
+        Arrays.sort(persons, (a, b)-> {
+            return a.getAge() < b.getAge() ? -1 : 0;
+        });
 
         assertArrayEquals(new Person[]{
             new Person("Иван", "Мельников", 20),
@@ -31,6 +36,9 @@ public class Exercise3 {
         Person[] persons = getPersons();
 
         // TODO использовать Arrays.sort + statement-lambda
+        Arrays.sort(persons, (a, b)-> a.getLastName().equals(b.getLastName()) ?
+                a.getFirstName().compareTo(b.getFirstName()) :
+                a.getLastName().compareTo(b.getLastName()));
 
         assertArrayEquals(new Person[]{
             new Person("Алексей", "Доренко", 40),
@@ -45,7 +53,7 @@ public class Exercise3 {
         List<Person> persons = Arrays.asList(getPersons());
 
         // TODO использовать FluentIterable
-        Person person = null;
+        Person person = from(persons).filter(person1 -> person1.getAge() == 30).first().get();
 
         assertEquals(new Person("Николай", "Зимов", 30), person);
     }
